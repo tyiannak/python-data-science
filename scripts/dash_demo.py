0ff9693a8f0566df8ad69c6d9b310b51c7425699
Theodoros Giannakopoulos <tyiannak@gmail.com>
2021-08-24 00:05:33 +0300

    refactor

--- scripts/dash_demo.py
+++ scripts/dash_demo.py
@@ -19,40 +19,28 @@ import numpy as np
 colors = {'background': '#111111', 'text': '#7FDBDD'}
 def_font = dict(family="Courier New, Monospace", size=10, color='#000000')
 
+
+# read airbnb data for Athens:
 csv_data = pd.read_csv("listings.csv")
 nei = csv_data["neighbourhood"].dropna().unique()
 nei_list = [{'label': c, 'value': c} for c in nei if c != "nan"]
-
-for n in nei:
-    print(n, len(csv_data[csv_data["neighbourhood"]==n]))
-
+#for n in nei:
+#    print(n, len(csv_data[csv_data["neighbourhood"]==n]))
 data = {'lon': np.array(csv_data['longitude']),
         'lat': np.array(csv_data['latitude'])}
 
-def draw_data():
-    print(data)
-    figure = {'data': [
-                        go.Scattermapbox(lat=data['lat'],  lon=data['lon'],
-                                   mode='markers',
-                                   marker_size=14,
-                                   marker_color='rgba(22, 182, 255, .9)'
-                                   ),
-                   ],
-        'layout': go.Layout(
-            hovermode='closest',
-            mapbox=dict(
-                accesstoken=open("mapbox_token").read(),
-                style='light',
-                bearing=0,
-                center=go.layout.mapbox.Center(
-                    lat=45,
-                    lon=-73
-                ),
-                pitch=0,
-                zoom=5
-            )
-           )}
 
+def draw_data():
+    figure = {'data': [go.Scattermapbox(lat=data['lat'],  lon=data['lon'],
+                                        mode='markers',  marker_size=10,
+                                        marker_color='rgba(22, 182, 255, .9)'),
+                       ],
+              'layout': go.Layout(
+                  hovermode='closest',
+                  mapbox=dict(accesstoken=open("mapbox_token").read(),
+                              style='light', bearing=0,
+                              center=go.layout.mapbox.Center( lat=45, lon=-73),
+                              pitch=0, zoom=5))}
     return dcc.Graph(figure=figure)
 
 
@@ -64,34 +52,31 @@ def get_layout():
 
     layout = dbc.Container([
         # Title
-        dbc.Row(dbc.Col(html.H2("Basic Classification Training Simulations",
+        dbc.Row(dbc.Col(html.H2("Airbnb Data Visualization Example",
                                 style={'textAlign': 'center',
                                        'color': colors['text']}))),
 
+        # Main Graph
         dbc.Row(
             [
                 dbc.Col(
-                    dcc.Graph(
-                        figure={'data': [go.Scatter(x=[1], y=[1], name='F')]}),
+                    dcc.Graph(figure={'data': [go.Scatter(x=[1], y=[1],
+                                                          name='F')]}),
                     id="main_graph"),
+            ], className="h-75"),
 
-            ], className="h-1"),
-
+        # Controls
         dbc.Row(
             [
                 html.Div([
-                    dcc.Dropdown(
-                        id='demo-dropdown',
-                        options=nei_list,
-                        value=nei[0]
-                    ),
+                    dcc.Dropdown(id='demo-dropdown', options=nei_list,
+                                 value=nei[0]),
                     html.Div(id='dd-output-container')
                 ]),
-                dbc.Col(html.Button('Perceptron Step', id='btn-next',
+                dbc.Col(html.Button('Run', id='btn-next',
                                     n_clicks=0)),
                 html.Div(id='container-button-timestamp')
-            ], className="h-1"),
-
+            ], className="h-25"),
     ], style={"height": "100vh"})
 
     return layout
@@ -101,7 +86,6 @@ if __name__ == '__main__':
     app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
     app.layout = get_layout()
-
 
     @app.callback(
         dash.dependencies.Output('dd-output-container', 'children'),
